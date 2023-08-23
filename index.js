b5a0f842345080e5895d4b3f43cb196ccea62c50
Sakhawat Hosen <sakhawathosen320@gmail.com>
2023-08-24 01:24:35 +0600

    onkdin pore error solve korlam and im comming back project

--- index.js
+++ index.js
@@ -10,22 +10,7 @@ const port = process.env.PORT||5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req,res,next)=>{
-  const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error:true,message:'unauthorized access'})
-  }
-  const token = authorization.split('')[1];
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-    if(err){
-      return res.status(401).send({err:true,message:'unauthorized access'})
-    }
-    req.decoded=decoded;
-    next()
-  })
 
-
-}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tw8naco.mongodb.net/?retryWrites=true&w=majority`;
@@ -47,7 +32,24 @@ async function run() {
     const userCollection = client.db("joka10resturent").collection("users");
     const reviewsCollection = client.db("joka10resturent").collection("reviews");
     const cartsCollection = client.db("joka10resturent").collection("carts");
-  
+    
+    
+    const verifyJWT = (req,res,next)=>{
+      const authorization = req.headers.authorization;
+      if(!authorization){
+        return res.status(401).send({error:true,message:'unauthorized access'})
+      }
+      const token = authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({err:true,message:'unauthorized access'})
+        }
+        req.decoded=decoded;
+        next()
+      })
+    
+    
+    }
     //  jwt post method
     app.post('/jwt',(req,res)=>{
       const user = req.body;
@@ -66,11 +68,11 @@ async function run() {
       }
       next();
     }
-
-    app.get('/users', verifyJWT,verifyAdmin,async(req,res)=>{
+    // verifyJWT, 
+    app.get('/users', verifyJWT,verifyAdmin, async(req,res)=>{
       const result = await userCollection.find().toArray();
       res.send(result)
-    })
+    });
 
     app.post('/users',async (req,res)=>{
       const user = req.body;
@@ -99,8 +101,8 @@ async function run() {
        res.send(result);
 
     })
-
-    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+    // verifyJWT,
+    app.get('/users/admin/:email',verifyJWT,   async (req, res) => {
       const email = req.params.email;
 
       if (req.decoded.email !== email) {
@@ -128,14 +130,15 @@ async function run() {
         res.send(result);
         
     });
-    app.get('/carts', verifyJWT,async(req,res)=>{
+    // verifyJWT
+    app.get('/carts',verifyJWT, async(req,res)=>{ 
       const email = req.query.email;
       if(!email){
         res.send([]);
       }
       const decodedEmail = req.decoded.email;
       if(email !== decodedEmail){
-        return res.status(403).send({error:true,message:'probiden access'})
+        return res.status(403).send({error:true,message:'forbidden access'})
       }
       const query = {email:email};
       const result = await cartsCollection.find(query).toArray();
